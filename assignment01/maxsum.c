4795e33ab7b6ab50fcb0b210d19b0ca29c303a8c
Daniel Szukitsch <daniel@szuuuken.at>
2017-03-22 13:38:08 +0100

    DEBUG infos added

--- assignment01/maxsum.c
+++ assignment01/maxsum.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
+/* === Macros === */
+
+#ifdef ENDEBUG
+#define DEBUG(...) do { fprintf(stderr, __VA_ARGS__); } while(0)
+#else
+#define DEBUG(...)
+#endif
+
+/* === Prototypes === */
+
 int maxsum (int max, int val);
+
+/* === Implementations === */
 
 int main (void){
 	int result0 = maxsum(0,0);
-	int result1 = maxsum(0,-1);
-	int result2 = maxsum(10,1);
-
 	(void) printf("maxsum(0,0) returns %i expected was 0\n", result0 );
+	DEBUG("\n");
+
+	int result1 = maxsum(0,-1);
 	(void) printf("maxsum(0,-1) returns %i expected was 0\n", result1 );
+	DEBUG("\n");
+
+	int result2 = maxsum(10,1);
 	(void) printf("maxsum(10,1) returns %i expected was 1\n", result2 );
 
 	return 0;
 }
 
 int maxsum (int max, int val) {
+	DEBUG("##########maxsum(%i,%i)##########\n",max,val);
 	int result = 0;
 	int i = 0;
 
@@ -24,6 +40,7 @@ int maxsum (int max, int val) {
 	while ((i < val) && (result <= max)) {
 		i = i + 1;
 		result = result + i;
+		DEBUG("i=%i result=%i\n",i,result);
 	}
 
 	if (result <= max)
